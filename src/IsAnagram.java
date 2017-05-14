0fc7ff264a05180af0c6dcf5e6fce75088a56844
TonyYutaka <yutakatony@gmail.com>
2017-05-14 11:07:26 -0700

    Anagram class & IsAnagram for test init

---
+++ src/IsAnagram.java
@@ -0,0 +1,8 @@
+
+public class IsAnagram {
+	
+	public static void main(String[] args) {
+		System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+	}
+	
+}
