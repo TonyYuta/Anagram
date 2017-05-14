5dea05215b2d0f43a8a41335a3687b4e81deaf1a
TonyYutaka <yutakatony@gmail.com>
2017-05-14 11:18:38 -0700

    TC res true

--- src/IsAnagram.java
+++ src/IsAnagram.java
@@ -3,11 +3,11 @@ public class IsAnagram {
 	
 	public static void main(String[] args) {
 		// anagram
-		//System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
+		System.out.println("Debit Card and Bad Credit is anagram? - " + Anagram.validate("Debit Card", "Bad Credit"));
 		
 		
 		// not an anagram because of !
-		System.out.println("Payment received! and Every cent paid me is anagram? - " + Anagram.validate("Payment received!", "Every cent paid me"));
+		//System.out.println("Payment received! and Every cent paid me is anagram? - " + Anagram.validate("Payment received!", "Every cent paid me"));
 
 		
 	}
